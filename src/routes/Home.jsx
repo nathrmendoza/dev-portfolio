324f5239d752d271b7a6eb3c331658b7537fa86c
Nath Mendoza <mendoza.nathaniel328@gmail.com>
2024-10-17 15:11:22 +1300

    updates: home description

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,7 +6,7 @@ import TextTransition, { presets } from 'react-text-transition'
 import { MainContainer, MediumWrapper } from "../styles/ThemeContainers";
 import PageAnimWrapper from "../utils/PageAnimWrapper";
 
-const HEROTEXT = ['veloper', 'signer', 'ployer', 'v Ops', '_dust2'];
+const HEROTEXT = ['veloper', 'signer', 'v Ops'];
 
 const Home = () => {  const [textIndex, setTextIndex] = useState(0);
   
@@ -20,9 +20,9 @@ const Home = () => {  const [textIndex, setTextIndex] = useState(0);
         <HeroWrapper>
             <PageAnimWrapper>
                 <MediumWrapper>
-                    <CustomHeroHeading>Nath the De<TextTransition springConfig={presets.gentle}>{HEROTEXT[textIndex % HEROTEXT.length]}</TextTransition></CustomHeroHeading>
+                    <CustomHeroHeading>Nath De<TextTransition springConfig={presets.gentle}>{HEROTEXT[textIndex % HEROTEXT.length]}</TextTransition></CustomHeroHeading>
                     <Paragraph style={{textAlign: 'center'}}>
-                        Hey there, I'm Nathaniel Mendoza, or just Nath for short, a UI/UX Developer located in Rolleston, New Zealand. My expertise lies in crafting web applications that provide digital services, ranging in complexity. I thrive on the challenge of constructing projects from scratch and enjoy finding inventive/unique solutions to problems along the way.
+                        Hey there, I'm Nathaniel Mendoza, or just Nath for short, a UI/UX Developer based in Waitomo Caves, New Zealand. My expertise lies in crafting web applications that provide digital services, ranging in complexity. I thrive on the challenge of constructing projects from scratch and enjoy finding inventive/unique solutions to problems along the way.
                     </Paragraph>
                 </MediumWrapper>
             </PageAnimWrapper>
